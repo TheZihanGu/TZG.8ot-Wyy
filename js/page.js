d6bbfbd21915a4eedeff496a512c5ac7ea2c6788
TheZihanGu <i@zihangu.cn>
2020-07-29 19:52:08 +0800

    Initial commit

---
+++ js/page.js
@@ -0,0 +1,18 @@
+var result = new Vue({
+    el: '#result',
+    data: {
+        message: '生不出人，我很抱歉。'
+    }
+})
+function getResult() {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', "https://api.tzg6.com/api/wyy", true);
+    httpRequest.send();
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            var json = eval('(' + httpRequest.responseText + ')');
+            console.log(json[0].text)
+            result.message = json[0].text
+        }
+    }
+}
